d6a914a8eba2546789d71465ee28d21b32e7b05a
Lucas Albuquerque <luucas.albuquerque23@gmail.com>
2025-09-22 14:08:54 -0300

    feat: Creating a header , separating a partial for heade and using include of template

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'recipes/pages/home.html')
 
-
-def contato(request):
-    return HttpResponse("contato")
-    ...
-
-def sobre(request):
-    return HttpResponse("sobre")
-    ...
